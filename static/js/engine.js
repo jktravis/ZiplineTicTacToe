374c48769f5c4cb698a82280385bea4bb813fc7b
jktravis <joshua.travis@gmail.com>
2016-03-19 07:34:07 -0400

    Added better win condition and state management.

--- static/js/engine.js
+++ static/js/engine.js
@@ -1,6 +1,7 @@
 exports.ticTacToe = {
   board: [],
   turn: '',
+  result: '',
 
   createBoard: function () {
     for (var i = 0; i < 9; i++) {
@@ -24,6 +25,9 @@ exports.ticTacToe = {
   },
 
   _isValidMove: function (move) {
+    if (this.isGameOver()) {
+      return false;
+    }
     return this.board[move] === null;
   },
 
@@ -32,30 +36,64 @@ exports.ticTacToe = {
   },
 
   _getNextMove: function () {
-    if (this.isWon()) {
-      return -1;
-    } else {
-      var move = Math.floor(Math.random() * 9);
-      var count = 0;
-      while (!this._isValidMove(move) && count < 8) {
-        move = Math.floor(Math.random() * 9);
-        count++;
-      }
-      return move;
+    if (this.isGameOver() === true) {
+      return true;
     }
+    var move = Math.floor(Math.random() * 9);
+    var count = 0;
+    while (!this._isValidMove(move) && count < 8) {
+      move = Math.floor(Math.random() * 9);
+      count++;
+    }
+    return move;
   },
 
-  isWon: function () {
-    return !!((this.board[0] === this.board[1] && this.board[1] === this.board[2]) || // check rows
-    (this.board[3] === this.board[4] && this.board[4] === this.board[5]) ||
-    (this.board[6] === this.board[7] && this.board[7] === this.board[8]) ||
+  emptyCells: function () {
+    var indxs = [];
+    for (var itr = 0; itr < 9; itr++) {
+      if (this.board[itr] === null) {
+        indxs.push(itr);
+      }
+    }
+    return indxs;
+  },
+
+  isGameOver: function () {
+    var B = this.board, i;
+
+    //check rows
+    for (i = 0; i <= 6; i = i + 3) {
+      if (B[i] !== null && B[i] === B[i + 1] && B[i + 1] == B[i + 2]) {
+        this.result = B[i] + "-won"; //update the state result
+        return true;
+      }
+    }
+
     //check columns
-    (this.board[0] === this.board[3] && this.board[3] === this.board[6]) ||
-    (this.board[1] === this.board[4] && this.board[4] === this.board[7]) ||
-    (this.board[2] === this.board[5] && this.board[5] === this.board[8]) ||
+    for (i = 0; i <= 2; i++) {
+      if (B[i] !== null && B[i] === B[i + 3] && B[i + 3] === B[i + 6]) {
+        this.result = B[i] + "-won"; //update the state result
+        return true;
+      }
+    }
+
     //check diagonals
-    (this.board[0] === this.board[4] && this.board[4] === this.board[8]) ||
-    (this.board[2] === this.board[4] && this.board[4] === this.board[6]));
+    for (i = 0, j = 4; i <= 2; i = i + 2, j = j - 2) {
+      if (B[i] !== null && B[i] == B[i + j] && B[i + j] === B[i + 2 * j]) {
+        this.result = B[i] + "-won"; //update the state result
+        return true;
+      }
+    }
+
+    var available = this.emptyCells();
+    if (available.length == 0) {
+      //the game is draw
+      this.result = "draw"; //update the state result
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 };
 
--- static/js/main.js
+++ static/js/main.js
@@ -11,15 +11,17 @@ $(document).ready(function() {
 
   squares.click(function(){
     var $this = $(this);
-    if ($this.text() !== '&nbsp;')
-    {
-      if (TicTacToe.makeMove(this.id)) {
-        player.turn = false;
-        $this.text('X');
-        var nextMove = TicTacToe._getNextMove();
-        TicTacToe.makeMove(nextMove);
-        $('#' + nextMove).text('O');
-        player.turn = true;
+    if (!TicTacToe.result.match(/(draw|won)/g) || TicTacToe.emptyCells() !== 0) {
+      if ($this.text() !== '&nbsp;') {
+        if (TicTacToe.makeMove(this.id)) {
+          player.turn = false;
+          $this.text('X');
+
+          var nextMove = TicTacToe._getNextMove();
+          TicTacToe.makeMove(nextMove);
+          $('#' + nextMove).text('O');
+          player.turn = true;
+        }
       }
       else {
         alert("Invalid move." + this.id);
