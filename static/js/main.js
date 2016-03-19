0c6774a912da64a50c25bd2031191f1800e52e58
jktravis <joshua.travis@gmail.com>
2016-03-19 17:07:23 -0400

    Modal prompt added.

--- static/js/main.js
+++ static/js/main.js
@@ -1,34 +1,44 @@
-$(document).ready(function() {
+$(document).ready(function () {
   var TicTacToe = exports.ticTacToe;
   var squares = $('.square');
-  var player = {
-    token: 'X'
-  };
-
+  var player, ai;
 
   updateMessage("Shall we plan a game?");
-  //todo: prompt for token.
-  TicTacToe.createBoard();
-  TicTacToe.turn = 'X';
 
-  squares.click(function(){
-    var $this = $(this);
-    if (!TicTacToe.isGameOver()) {
-      if ($this.text() !== '&nbsp;') {
-        if (TicTacToe.makeMove(this.id)) {
-          $this.text('X');
+  $('.modal-body button').on('click', function () {
+    player = this.id;
 
-          var nextMove = TicTacToe._getNextMove();
-          if (TicTacToe.isGameOver() !== true) {
-            TicTacToe.makeMove(nextMove);
-            $('#' + nextMove).text('O');
+    if (player === 'X') {
+      ai = 'O';
+    }
+    else if (player === 'O') {
+      ai = 'X';
+    }
+
+    TicTacToe.createBoard();
+    TicTacToe.turn = player;
+
+    squares.on("click", function () {
+      var $this = $(this);
+      if (!TicTacToe.isGameOver()) {
+        if ($this.text() !== '&nbsp;') {
+          if (TicTacToe.makeMove(this.id)) {
+            $this.text(player);
+            aiPlay();
           }
         }
       }
-    }
-
-    updateMessage(TicTacToe.result);
+      updateMessage(TicTacToe.result);
+    });
   });
+
+  function aiPlay() {
+    var nextMove = TicTacToe._getNextMove();
+    if (TicTacToe.isGameOver() !== true) {
+      TicTacToe.makeMove(nextMove);
+      $('#' + nextMove).text(ai);
+    }
+  }
 
   function updateMessage(m) {
     $("#message").text(m);
