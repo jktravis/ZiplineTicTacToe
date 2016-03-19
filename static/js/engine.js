f8a3ce90099a1d4eccb832e1a1e9e459e3be1f53
jktravis <joshua.travis@gmail.com>
2016-03-18 23:12:03 -0400

    Added some win conditions and updated board to be a single dimension.

--- static/js/engine.js
+++ static/js/engine.js
@@ -3,8 +3,8 @@ exports.ticTacToe = {
   turn: '',
 
   createBoard: function () {
-    for (var i = 0; i < 3; i++) {
-      this.board.push([null, null, null]);
+    for (var i = 0; i < 9; i++) {
+      this.board.push(null);
     }
     return this.board;
   },
@@ -13,8 +13,8 @@ exports.ticTacToe = {
     if (this.turn === '') {
       throw Error('Turn value cannot be empty.');
     }
-    if (this._isValidMove(move, this.board)) {
-      this.board[move[0]][move[1]] = this.turn;
+    if (this._isValidMove(move)) {
+      this.board[move] = this.turn;
       this._switchTurn();
       return true;
     }
@@ -24,7 +24,7 @@ exports.ticTacToe = {
   },
 
   _isValidMove: function (move) {
-    return this.board[move[0]][move[1]] === null;
+    return this.board[move] === null;
   },
 
   _switchTurn: function () {
@@ -32,7 +32,28 @@ exports.ticTacToe = {
   },
 
   _getNextMove: function () {
+    if (!this.isWon()) {
+      var move = Math.floor(Math.random() * 9);
+      var count = 0;
+      while (!this._isValidMove(move) && count < 8) {
+        move = Math.floor(Math.random() * 9);
+        count++;
+      }
+      return move;
+    }
+  },
 
+  isWon: function () {
+    return !!((this.board[0] === this.board[1] && this.board[1] === this.board[2]) || // check rows
+    (this.board[3] === this.board[4] && this.board[4] === this.board[5]) ||
+    (this.board[6] === this.board[7] && this.board[7] === this.board[8]) ||
+    //check columns
+    (this.board[0] === this.board[3] && this.board[3] === this.board[6]) ||
+    (this.board[1] === this.board[4] && this.board[4] === this.board[7]) ||
+    (this.board[2] === this.board[5] && this.board[5] === this.board[8]) ||
+    //check diagonals
+    (this.board[0] === this.board[4] && this.board[4] === this.board[8]) ||
+    (this.board[2] === this.board[4] && this.board[4] === this.board[6]));
   }
 };
 
