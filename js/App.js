cc1431cced4f4989f50782b35a74cfdf21bcd459
jktravis <joshua.travis@gmail.com>
2017-09-06 20:31:19 -0400

    Added basic modal.

--- js/App.js
+++ js/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col, Button } from 'react-bootstrap';
+import { Grid, Row, Col, Button, Modal } from 'react-bootstrap';
 import Square from './Square';
 import TypeText from './TypeText';
 import TicTacToe from './TicTacToe';
@@ -9,9 +9,20 @@ class App extends Component {
     super();
 
     this.state = TicTacToe.reset();
+    this.state.showChooser = false;
 
     this.handleSquareClick = this.handleSquareClick.bind(this);
     this.handleResetClick = this.handleResetClick.bind(this);
+    this.openChooser = this.openChooser.bind(this);
+    this.closeChooser = this.closeChooser.bind(this);
+  }
+
+  openChooser() {
+    this.setState(state => ({ showChooser: true }));
+  }
+
+  closeChooser() {
+    this.setState(state => ({ showChooser: false }));
   }
 
   handleSquareClick(event) {
@@ -59,46 +70,59 @@ class App extends Component {
     }).filter(datum => datum);
 
     return (
-      <Grid fluid>
-        <Row>
-          <Col md={4} lg={4} lgOffset={4} mdOffset={4} sm={10} smOffset={1} xs={12}>
-            <h1>Tic-Tac-Toe</h1>
-          </Col>
-        </Row>
-        <Row>
-          <Col lg={4} lgOffset={4} md={4} mdOffset={4} sm={4} smOffset={4}
-               xs={10} xsOffset={1}>
-            {groups.map((group, rIdx) => {
-              return (
-                <Row key={rIdx}>
-                  {group.map((cell, cIdx) => {
-                    return (
-                      <Square key={cell.id} onClick={cell.value ? null : this.handleSquareClick} id={cell.id}>
-                        {cell.value}
-                      </Square>
-                    );
-                  })}
-                </Row>
-              );
-            })}
-          </Col>
-        </Row>
-        <Row>
-          <Col lg={4} lgOffset={4} md={4} mdOffset={4} sm={4} smOffset={4}
-               xs={10} xsOffset={1}>
-            <TypeText strings={['Shall we play a game?']}/>
-          </Col>
-        </Row>
-        <Row>
-          <Col lg={4} lgOffset={4} md={4} mdOffset={4} sm={4} smOffset={4}
-               xs={10} xsOffset={1}>
-            <div id="buttons">
-              <Button bsStyle="success">Start</Button>
-              <Button bsStyle="warning" onClick={this.handleResetClick}>Reset</Button>
-            </div>
-          </Col>
-        </Row>
-      </Grid>
+      <div>
+        <Grid fluid>
+          <Row>
+            <Col md={4} lg={4} lgOffset={4} mdOffset={4} sm={10} smOffset={1} xs={12}>
+              <h1>Tic-Tac-Toe</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg={4} lgOffset={4} md={4} mdOffset={4} sm={4} smOffset={4}
+                 xs={10} xsOffset={1}>
+              {groups.map((group, rIdx) => {
+                return (
+                  <Row key={rIdx}>
+                    {group.map((cell) => {
+                      return (
+                        <Square key={cell.id} onClick={cell.value ? null : this.handleSquareClick} id={cell.id}>
+                          {cell.value}
+                        </Square>
+                      );
+                    })}
+                  </Row>
+                );
+              })}
+            </Col>
+          </Row>
+          <Row>
+            <Col lg={4} lgOffset={4} md={4} mdOffset={4} sm={4} smOffset={4}
+                 xs={10} xsOffset={1}>
+              <TypeText strings={['Shall we play a game?']}/>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg={4} lgOffset={4} md={4} mdOffset={4} sm={4} smOffset={4}
+                 xs={10} xsOffset={1}>
+              <div id="buttons">
+                <Button bsStyle="success" onClick={this.openChooser}>Start</Button>
+                <Button bsStyle="warning" onClick={this.handleResetClick}>Reset</Button>
+              </div>
+            </Col>
+          </Row>
+        </Grid>
+        <Modal show={this.state.showChooser} onHide={this.closeChooser}>
+          <Modal.Header closeButton>
+            <Modal.Title>Choose</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.closeChooser}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
     );
   }
 }
